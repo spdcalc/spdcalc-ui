f8f0b5de98e72a4cd10f135461dda062292f8fda
Jasper <well.caffeinated@gmail.com>
2019-05-24 10:36:04 -0600

    try using pointers

--- src/workers/test.js
+++ src/workers/test.js
@@ -1,13 +1,17 @@
 import { calcGaussian } from '@/benchmarks/simple'
+
 // import * as spdc from 'spdcalc'
 //
 // console.log(spdc)
 
 const spdcMod = import('spdcalc')
+const wasm = import('spdcalc/spdcalc_bg')
 
 export async function getGaussian( width, height ){
+  const memory = (await wasm).memory
   const spdc = await spdcMod
-  return spdc.get_gaussian( width, height )
+  let ptr = await spdc.get_gaussian( width, height )
+  return new Float64Array(memory.buffer, ptr, width * height)
 }
 
 export async function getGaussianJS( width, height ){
