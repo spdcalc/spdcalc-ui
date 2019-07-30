3ccd2db7b5cc230109687fe1b71d0280da4b3840
Jasper <jasper.palfree@colorado.edu>
2019-07-30 12:22:50 -0600

    add progress bar

--- src/components/site-footer.vue
+++ src/components/site-footer.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
-v-footer(app, dark)
-  v-container.py-0.white--text(fluid)
+v-footer(app, dark, padless, height="38")
+  v-progress-linear.progress(
+    indeterminate
+    , v-show="isLoading"
+  )
+  v-container.py-1.white--text(fluid)
     v-layout(columns)
       v-flex(xs6) Footer
       v-flex(xs6)
-        .text-right {{jobName}} took {{runTime.toFixed(2)}}ms
+        .text-right(v-if="isLoading") {{jobName}} calculating...
+        .text-right(v-else) {{jobName}} took {{runTime.toFixed(2)}}ms
 </template>
 
 <script>
@@ -22,9 +27,16 @@ export default {
     ...mapGetters('jobs', [
       'runTime'
       , 'jobName'
+      , 'isLoading'
     ])
   }
   , methods: {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.progress
+  position: absolute
+  top: 0
+</style>
