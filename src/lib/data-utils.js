b213657b50e0f1058a8605669579a90db4846ae9
Jasper <well.caffeinated@gmail.com>
2023-03-01 17:16:59 -0700

    better grouped array creation

--- src/lib/data-utils.js
+++ src/lib/data-utils.js
@@ -5,11 +5,9 @@ export function createGroupedArray(arr, chunkSize){
     for (let i = 0; i < len; i += chunkSize) {
       groups.push(arr.slice(i, i + chunkSize))
     }
-  } else {
-    let byteSize = arr.byteLength / chunkSize | 0
-    let nGroups = len / chunkSize | 0
-    for (let i = 0; i < nGroups; i++) {
-      groups.push(new Float64Array(arr.buffer, i * byteSize, chunkSize))
+  } else if (ArrayBuffer.isView(arr)) {
+    for (let i = 0; i < len; i += chunkSize) {
+      groups.push(arr.subarray(i, i + chunkSize))
     }
   }
   return groups
