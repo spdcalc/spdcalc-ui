75c0e7dc0bdee519e540e223e28b9f016c9a0392
Jasper Palfree <well.caffeinated@gmail.com>
2024-07-26 17:42:44 -0600

    formatting

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,29 @@
 // vite.config.js
-import pkg from './package.json'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue2'
-import path from 'path'
-import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
+import pkg from "./package.json";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue2";
+import path from "path";
+import { VuetifyResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
 // import wasm from "vite-plugin-wasm"
 // import wasmPack from 'vite-plugin-wasm-pack'
-import { ViteRsw } from 'vite-plugin-rsw'
+import { ViteRsw } from "vite-plugin-rsw";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? `/${pkg.name}/`
-    : '/',
+  base: process.env.NODE_ENV === "production" ? `/${pkg.name}/` : "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~': path.resolve(__dirname, 'node_modules')
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "node_modules"),
+    },
   },
   css: {
     preprocessorOptions: {
       sass: {
-        additionalData: `@import "@/variables.scss"\n`
-      }
-    }
+        additionalData: `@import "@/variables.scss"\n`,
+      },
+    },
   },
   plugins: [
     ViteRsw(),
@@ -33,14 +31,12 @@ export default defineConfig({
     Components({
       resolvers: [
         // Vuetify
-        VuetifyResolver()
-      ]
-    })
+        VuetifyResolver(),
+      ],
+    }),
   ],
   worker: {
     // type: 'es',
-    plugins: [
-      ViteRsw()
-    ]
-  }
-})
+    plugins: [ViteRsw()],
+  },
+});
