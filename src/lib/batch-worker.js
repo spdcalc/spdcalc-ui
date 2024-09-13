4f255475b7e7abdb597e144e0babc23bf532ddc4
Jasper Palfree <well.caffeinated@gmail.com>
2024-09-13 13:52:34 -0600

    fix: multiple plots of same kind prevent each other from loading

--- src/lib/batch-worker.js
+++ src/lib/batch-worker.js
@@ -7,7 +7,7 @@ import createWorkerQueue, { makeThenable } from './worker-queue'
 
 const cpuCores = navigator.hardwareConcurrency || 2
 
-const waiter = dt => {
+const waiter = (dt) => {
   const ret = {}
   let timeout
   ret.promise = new Promise((resolve) => {
@@ -20,17 +20,21 @@ const waiter = dt => {
 }
 
 export function interruptDebounce(fn, dt = 100, opts = null) {
-  let receipt
-  let wait
+  // Need to use weakmap here because component instances
+  // must have their own context for wait/receipt
+  const cache = new WeakMap()
   return async function exec(...args) {
-    wait?.cancel()
-    wait = waiter(dt)
-    await wait.promise
-    receipt?.cancel()
-    receipt = fn.apply(this, args)
-    if (!receipt){ return }
-    return receipt.catch(e => {
-      if (e.message === 'Cancelled'){
+    const r = cache.get(this) || {}
+    r.wait?.cancel()
+    r.wait = waiter(dt)
+    await r.wait.promise
+    r.receipt?.cancel()
+    r.receipt = fn.apply(this, args)
+    if (!r.receipt) {
+      return
+    }
+    return r.receipt.catch((e) => {
+      if (e.message === 'Cancelled') {
         return new Promise(() => {}) // never resolve
       }
       return Promise.reject(e)
@@ -40,8 +44,8 @@ export function interruptDebounce(fn, dt = 100, opts = null) {
 
 function concatenate(arrays) {
   let A = arrays[0] ? arrays[0].constructor : Array
-  if ( A.prototype.set ){
-    let totalLength = _sumBy(arrays, a => a.length)
+  if (A.prototype.set) {
+    let totalLength = _sumBy(arrays, (a) => a.length)
     let result = new A(totalLength)
     let offset = 0
     for (let arr of arrays) {
@@ -58,38 +62,41 @@ function concatenate(arrays) {
   }
 }
 
-export const concatResults = results => {
+export const concatResults = (results) => {
   // reassemble
   return concatenate(results)
   // return results.reduce((res, part) => res.concat(part), new A())
 }
 
-const timed = fn => (...args) => {
-  let start = performance.now()
-  return fn(...args).then(result => {
-    return {
-      result,
-      duration: performance.now() - start
-    }
-  })
-}
+const timed =
+  (fn) =>
+  (...args) => {
+    let start = performance.now()
+    return fn(...args).then((result) => {
+      return {
+        result,
+        duration: performance.now() - start,
+      }
+    })
+  }
 
-export function BatchWorker( factory, concurrency = cpuCores ){
+export function BatchWorker(factory, concurrency = cpuCores) {
   const workerQueue = createWorkerQueue(factory, concurrency)
+  const uuid = Math.random().toString(36).slice(2)
 
-  function destroy(){
-    log('Cleanup worker queue')
+  function destroy() {
+    log(`(worker: ${uuid}) Cleanup worker queue`)
     workerQueue.destroy()
   }
 
-  function run(method, args){
+  function run(method, args) {
     return workerQueue.enqueue((worker) => worker[method](...args))
   }
 
   const exec = timed((method, argList) => {
-    log(`Job (batch) queued ${method}:`, argList)
+    log(`(worker: ${uuid}) Job (batch) queued ${method}:`, argList)
 
-    const jobs = argList.map(args => run(method, args))
+    const jobs = argList.map((args) => run(method, args))
 
     const cancel = () => {
       for (let j of jobs) {
@@ -97,38 +104,39 @@ export function BatchWorker( factory, concurrency = cpuCores ){
       }
     }
 
-    const promise = Promise.all(jobs.map(j => j.promise)).catch(e => {
+    const promise = Promise.all(jobs.map((j) => j.promise)).catch((e) => {
       cancel()
       return Promise.reject(e)
     })
 
     return makeThenable({
       promise,
-      cancel
+      cancel,
     })
   })
 
-  function execAndConcat(method, argList){
+  function execAndConcat(method, argList) {
     const receipt = exec(method, argList)
     receipt.promise = receipt.promise.then(({ result, duration }) => ({
-      result: concatResults(result)
-      , duration
+      result: concatResults(result),
+      duration,
     }))
     return receipt
   }
 
   const execSingle = timed((method, ...args) => {
-    log(`Job queued ${method}:`, args)
+    log(`(worker: ${uuid}) Job queued ${method}:`, args)
     return run(method, args)
   })
 
   return {
-    exec
-    , execAndConcat
-    , execSingle
-    , partitionSteps: _partialRight(partitionSteps, concurrency)
-    , destroy
-    , length: concurrency
+    exec,
+    execAndConcat,
+    execSingle,
+    partitionSteps: _partialRight(partitionSteps, concurrency),
+    destroy,
+    length: concurrency,
+    uuid,
   }
 }
 
@@ -136,7 +144,7 @@ export function BatchWorker( factory, concurrency = cpuCores ){
 // range = [min, max]
 // steps, number of steps to take in that range
 // nPartitions number of partitions
-export function partitionSteps(range, steps, nPartitions){
+export function partitionSteps(range, steps, nPartitions) {
   let x0 = range[0]
   let dx = (range[1] - x0) / (steps - 1)
   let n = (steps / nPartitions) | 0
@@ -146,8 +154,8 @@ export function partitionSteps(range, steps, nPartitions){
     let min = x0 + (i * n + Math.min(i, remaining)) * dx
     let max = min + (steps - 1) * dx
     return {
-      range: [min, max]
-      , count: steps
+      range: [min, max],
+      count: steps,
     }
   })
 }
