debf4237c7971ae344dd00e343ed5b3086f4f3b3
Jasper Palfree <well.caffeinated@gmail.com>
2026-04-30 20:50:29 +0000

    fix: add documentation link

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,12 @@ v-app#app
             v-list-item-title Import Settings
           v-list-item(@click="exportSettingsDialog = true")
             v-list-item-title Export Settings
+          v-list-item(href="https://docs.spdcalc.org", target="_blank")
+            v-list-item-title Documentation
           v-list-item(href="https://v1.spdcalc.org", target="_blank")
             v-list-item-title SPDCalc v1
-          v-list-item(@click="aboutDialog = true")
-            v-list-item-title About
+          //- v-list-item(@click="aboutDialog = true")
+          //-   v-list-item-title About
           v-list-item(disabled)
             v-list-item-title build: {{ buildVersion }}
   v-main
